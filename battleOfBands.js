9a7b4599735e3a598ee270477d42cf8d307d2480
amazoradi <amazoradi@gmail.com>
2018-10-16 13:55:16 -0500

    initial function written

---
+++ battleOfBands.js
@@ -0,0 +1,8 @@
+let bandNumber = 1
+let takeNumber = function (name) {
+  console.log(bandNumber++ + ", " + name);
+};
+
+takeNumber("Metallica");
+takeNumber("Katty Purry");
+takeNumber("Tenacious D")
